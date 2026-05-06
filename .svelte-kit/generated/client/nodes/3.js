a823b2a91a2e595fa339ad228c523a604d30892a
h591305 <haavard.bydall@hotmail.com>
2026-05-06 21:03:54 +0200

    Initial commit

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/onskeliste/+page.ts";
+export { universal };
+export { default as component } from "../../../../src/routes/onskeliste/+page.svelte";
