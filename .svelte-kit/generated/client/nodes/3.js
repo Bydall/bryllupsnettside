b4244ba7bf1ffdaff4cdf749739a90b44c67bcc3
h591305 <haavard.bydall@hotmail.com>
2026-05-06 21:04:49 +0200

    Revert "Initial commit"
    This reverts commit a823b2a91a2e595fa339ad228c523a604d30892a.

--- .svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/onskeliste/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/onskeliste/+page.svelte";
